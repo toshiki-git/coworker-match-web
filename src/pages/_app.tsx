31a6b57f73a4af8f29927e84657cba2a27f6c26f
Toshiki Takakubo <219tosiki@gmail.com>
2024-06-19 14:58:50 +0900

    feat: add enableMocking function

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css';
 import { Noto_Sans_JP } from 'next/font/google';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
-import { useEffect } from 'react';
+import { enableMocking } from '@/mocks/enableMocking';
 
 const notoSansJP = Noto_Sans_JP({
   weight: ['400', '700'],
@@ -10,23 +10,9 @@ const notoSansJP = Noto_Sans_JP({
   variable: '--font-noto-sans-jp',
 });
 
-async function enableMocking() {
-  if (process.env.NODE_ENV !== 'development') {
-    return;
-  }
-
-  const { worker } = await import('@/mocks/browser');
-
-  // `worker.start()` returns a Promise that resolves
-  // once the Service Worker is up and ready to intercept requests.
-  return worker.start();
-}
+enableMocking();
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    enableMocking();
-  }, []);
-
   return (
     <SessionProvider session={pageProps.session}>
       <div className={notoSansJP.className}>
