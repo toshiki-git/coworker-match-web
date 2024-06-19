4e05ca22e62e36cede9f2ab3c0fe8c4d06349138
Toshiki Takakubo <219tosiki@gmail.com>
2024-06-19 15:26:39 +0900

    feat: Add questions fixture data

---
+++ src/mocks/fixtures/questions.ts
@@ -0,0 +1,62 @@
+export const questions = [
+  {
+    question_id: '1',
+    question_text: 'あなたの出身地はどこですか？',
+    choice1: {
+      choice_text: '東京',
+      choice_image_url: 'https://www.svgrepo.com/show/530661/genetic-data.svg',
+    },
+    choice2: {
+      choice_text: '大阪',
+      choice_image_url: 'https://www.svgrepo.com/show/530661/genetic-data.svg',
+    },
+  },
+  {
+    question_id: '2',
+    question_text: 'あなたの職種は？',
+    choice1: {
+      choice_text: 'エンジニア',
+      choice_image_url: 'https://www.svgrepo.com/show/530661/genetic-data.svg',
+    },
+    choice2: {
+      choice_text: 'デザイナー',
+      choice_image_url: 'https://www.svgrepo.com/show/530661/genetic-data.svg',
+    },
+  },
+  {
+    question_id: '3',
+    question_text: '好きな趣味は何ですか？',
+    choice1: {
+      choice_text: 'ゲーム',
+      choice_image_url: 'https://www.svgrepo.com/show/530661/genetic-data.svg',
+    },
+    choice2: {
+      choice_text: 'ランニング',
+      choice_image_url: 'https://www.svgrepo.com/show/530661/genetic-data.svg',
+    },
+  },
+  {
+    question_id: '4',
+    question_text: '好きな食べ物は？',
+    choice1: {
+      choice_text: '寿司',
+      choice_image_url: 'https://www.svgrepo.com/show/530661/genetic-data.svg',
+    },
+    choice2: {
+      choice_text: 'ピザ',
+      choice_image_url: 'https://www.svgrepo.com/show/530661/genetic-data.svg',
+    },
+  },
+  {
+    question_id: '5',
+    question_text: '週末に何をしますか？',
+    choice1: {
+      choice_text: '映画を見る',
+      choice_image_url: 'https://www.svgrepo.com/show/530661/genetic-data.svg',
+    },
+    choice2: {
+      choice_text: '旅行する',
+      choice_image_url: 'https://www.svgrepo.com/show/530661/genetic-data.svg',
+    },
+  },
+];
