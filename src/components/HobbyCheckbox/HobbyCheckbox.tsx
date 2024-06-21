bcafe84d1423a06fa8507c71021ee78f1c5d123c
Toshiki Takakubo <219tosiki@gmail.com>
2024-06-21 14:30:53 +0900

    refactor: Add onClick event to HobbyCheckbox component

--- src/components/HobbyCheckbox/HobbyCheckbox.tsx
+++ src/components/HobbyCheckbox/HobbyCheckbox.tsx
@@ -3,11 +3,15 @@ import { Checkbox } from '@/components/ui/checkbox';
 interface HobbyCheckboxProps {
   id: string;
   label: string;
+  onChange: (id: string) => void;
 }
 
-export function HobbyCheckbox({ id, label }: HobbyCheckboxProps) {
+export function HobbyCheckbox({ id, label, onChange }: HobbyCheckboxProps) {
   return (
-    <button className="flex items-center space-x-2 border border-spacing-2 rounded-full p-3 bg-white border-green-700 hover:bg-green-100">
+    <button
+      className="flex items-center space-x-2 border border-spacing-2 rounded-full p-3 bg-white border-green-700 hover:bg-green-100"
+      onClick={() => onChange(id)}
+    >
       <Checkbox id={id} />
       <label
         htmlFor={id}
