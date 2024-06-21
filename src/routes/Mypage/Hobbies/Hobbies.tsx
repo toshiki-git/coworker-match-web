ad4fc1fa39d44c47580bdd258a1f659528b6146d
Toshiki Takakubo <219tosiki@gmail.com>
2024-06-21 10:20:37 +0900

    feat: create UserHobbies page templete

---
+++ src/routes/Mypage/Hobbies/Hobbies.tsx
@@ -0,0 +1,10 @@
+import { Layout } from '@/layouts';
+
+export function HobbiesPage() {
+  return (
+    <Layout>
+      <h1>Hobbies</h1>
+      <p>What are your hobbies?</p>
+    </Layout>
+  );
+}
