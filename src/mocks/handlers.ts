bda100a0f0518ab2fbbacffaf1e5b1504f519479
Toshiki Takakubo <219tosiki@gmail.com>
2024-06-21 14:31:13 +0900

    feat: Add hobbies fixture data and update handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,9 @@
 import { http, HttpResponse } from 'msw';
 import { questions } from '@/mocks/fixtures/questions';
+import { hobbies } from '@/mocks/fixtures/hobbies';
 
 export const handlers = [
   http.get('/resource', () => HttpResponse.json({ id: 'abc-123' })),
   http.get('/questions', () => HttpResponse.json(questions)),
+  http.get('/hobbies', () => HttpResponse.json(hobbies)),
 ];
