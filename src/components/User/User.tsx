83485aa7fe8329385090d65e3e1331ca87556542
Toshiki Takakubo <219tosiki@gmail.com>
2024-06-21 18:03:48 +0900

    feat: Add User component for displaying user information

---
+++ src/components/User/User.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image';
+
+interface UserProps {
+  image_url: string;
+  userName: string;
+  message: string;
+  unreadCount: number;
+}
+
+export function User({ image_url, userName, message, unreadCount }: UserProps) {
+  return (
+    <div className="cursor-pointer max-w-md mx-auto flex items-center p-4 border-b border-gray-300 bg-white hover:bg-gray-100 transition duration-200 ease-in-out">
+      <div className="flex-shrink-0">
+        <Image
+          src={image_url}
+          alt={`${userName} avatar`}
+          width={50}
+          height={50}
+          className="rounded-full"
+        />
+      </div>
+      <div className="ml-4 flex-1 overflow-hidden">
+        <div className="flex items-center justify-between">
+          <h3 className="text-lg font-medium text-gray-900 truncate">
+            {userName}
+          </h3>
+        </div>
+        <p className="text-sm text-gray-600 mt-1 truncate">{message}</p>
+      </div>
+      {unreadCount > 0 && (
+        <div
+          className={`ml-2 text-sm text-white bg-orange-500 ${unreadCount < 10 ? 'w-6 h-6' : 'px-3 py-1'} rounded-full shadow-md flex items-center justify-center`}
+        >
+          {unreadCount}
+        </div>
+      )}
+    </div>
+  );
+}
--- src/routes/Matchings/Matchings.tsx
+++ src/routes/Matchings/Matchings.tsx
@@ -1,3 +1,19 @@
+import { User } from '@/components/User';
+import { Layout } from '@/layouts';
+import { useSession } from 'next-auth/react';
+
 export function MatchingsPage() {
-  return <div>MatchingsPage</div>;
+  const { data: session } = useSession();
+  return (
+    <Layout>
+      <div className="container mt-5">
+        <User
+          image_url={session?.user?.image ?? ''}
+          userName="User NameUser NameUser NameUser NameUser NameUser Name"
+          message="Message here Message hereMessage here Message hereMessage here Message hereMessage here Message here"
+          unreadCount={115}
+        />
+      </div>
+    </Layout>
+  );
 }
