dd6fec2ef573cfcf3220612dc8742e677eadb188
Toshiki Takakubo <219tosiki@gmail.com>
2024-06-23 21:28:44 +0900

    feat: create Message Input component

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,24 +1,6 @@
 import { useState } from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from '@/components/ui/form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
-import { toast } from '@/components/ui/use-toast';
-
-const FormSchema = z.object({
-  message: z.string().min(1, {
-    message: '回答を入力してください。',
-  }),
-});
+import { MessageInput } from '@/components/MessageInput';
 
 interface MessageProps {
   message_id: string;
@@ -37,58 +19,7 @@ export function Message({
   other_icon_url,
   other_answer,
 }: MessageProps) {
-  const [answer, setAnswer] = useState(my_answer);
-
-  const handleAnswerSubmit = async (answer: string) => {
-    try {
-      await fetch(`/messages/${message_id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ my_answer: answer }),
-      });
-      setAnswer(answer);
-    } catch (error) {
-      console.error('Error updating answer:', error);
-    }
-  };
-
-  function InputForm() {
-    const form = useForm<z.infer<typeof FormSchema>>({
-      resolver: zodResolver(FormSchema),
-    });
-
-    const onSubmit = (data: z.infer<typeof FormSchema>) => {
-      toast({
-        title: 'メッセージが送信されました',
-      });
-      handleAnswerSubmit(data.message);
-    };
-
-    return (
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="flex w-full max-w-sm items-center space-x-2"
-        >
-          <FormField
-            control={form.control}
-            name="message"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input placeholder="回答を入力してください" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">送信</Button>
-        </form>
-      </Form>
-    );
-  }
+  const [answer, setAnswer] = useState<string>(my_answer);
 
   return (
     <div className="w-[40rem] flex flex-col space-y-4 p-4 mb-2">
@@ -96,7 +27,7 @@ export function Message({
       <div className="flex items-center justify-end space-x-2">
         <div className="bg-green-200 p-4 rounded-lg shadow-md max-w-sm text-right">
           {answer === '' ? (
-            <InputForm />
+            <MessageInput message_id={message_id} setAnswer={setAnswer} />
           ) : (
             <p className="text-gray-700">{answer}</p>
           )}
---
+++ src/components/MessageInput/MessageInput.tsx
@@ -0,0 +1,75 @@
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { Input } from '@/components/ui/input';
+import { toast } from '@/components/ui/use-toast';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const FormSchema = z.object({
+  message: z.string().min(1, {
+    message: '回答を入力してください。',
+  }),
+});
+
+interface MessageInputProps {
+  message_id: string;
+  setAnswer: (answer: string) => void;
+}
+
+export function MessageInput({ message_id, setAnswer }: MessageInputProps) {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const handleAnswerSubmit = async (answer: string) => {
+    try {
+      await fetch(`/messages/${message_id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ my_answer: answer }),
+      });
+      setAnswer(answer);
+    } catch (error) {
+      console.error('Error updating answer:', error);
+    }
+  };
+
+  const onSubmit = (data: z.infer<typeof FormSchema>) => {
+    toast({
+      title: 'メッセージが送信されました',
+    });
+    handleAnswerSubmit(data.message);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex w-full max-w-sm items-center space-x-2"
+      >
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="回答を入力してください" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">送信</Button>
+      </form>
+    </Form>
+  );
+}
