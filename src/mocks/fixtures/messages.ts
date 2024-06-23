1f41758bc47d559ed9d27d60eec7013eb331be91
Toshiki Takakubo <219tosiki@gmail.com>
2024-06-23 18:27:44 +0900

    feat: Add new messages to messages fixture

--- src/mocks/fixtures/messages.ts
+++ src/mocks/fixtures/messages.ts
@@ -1,5 +1,6 @@
 export const messages = [
   {
+    message_id: '3fa85f64-5717-4562-b3fc-2c963f66afa0',
     question_text: '子供のころの夢は何ですか？',
     my_answer: 'サッカー選手',
     my_icon_url: '/icon_sample.jpg',
@@ -7,6 +8,7 @@ export const messages = [
     other_icon_url: '/icon_sample.jpg',
   },
   {
+    message_id: '3fa85f64-5717-4562-b3fc-2c963f66afa1',
     question_text: 'どこに住みたいですか？',
     my_answer: 'パリ',
     my_icon_url: '/icon_sample.jpg',
@@ -14,6 +16,7 @@ export const messages = [
     other_icon_url: '/icon_sample.jpg',
   },
   {
+    message_id: '3fa85f64-5717-4562-b3fc-2c963f66afa2',
     question_text: '好きな本は何ですか？',
     my_answer: 'ハリー・ポッター',
     my_icon_url: '/icon_sample.jpg',
@@ -21,6 +24,7 @@ export const messages = [
     other_icon_url: '/icon_sample.jpg',
   },
   {
+    message_id: '3fa85f64-5717-4562-b3fc-2c963f66afa3',
     question_text: '最近楽しかったことは？',
     my_answer: '友達とキャンプ',
     my_icon_url: '/icon_sample.jpg',
@@ -28,6 +32,7 @@ export const messages = [
     other_icon_url: '/icon_sample.jpg',
   },
   {
+    message_id: '3fa85f64-5717-4562-b3fc-2c963f66afa4',
     question_text: '密かな楽しみは？',
     my_answer: '夜中のスナック',
     my_icon_url: '/icon_sample.jpg',
@@ -35,6 +40,7 @@ export const messages = [
     other_icon_url: '/icon_sample.jpg',
   },
   {
+    message_id: '3fa85f64-5717-4562-b3fc-2c963f66afa5',
     question_text: '旅行するならどの国に行ってみたいですか？',
     my_answer: 'イタリア',
     my_icon_url: '/icon_sample.jpg',
@@ -42,6 +48,7 @@ export const messages = [
     other_icon_url: '/icon_sample.jpg',
   },
   {
+    message_id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
     question_text: '一番好きな食べ物は何ですか？',
     my_answer: '寿司',
     my_icon_url: '/icon_sample.jpg',
@@ -49,6 +56,7 @@ export const messages = [
     other_icon_url: '/icon_sample.jpg',
   },
   {
+    message_id: '3fa85f64-5717-4562-b3fc-2c963f66afa7',
     question_text: 'もし1億円もらったら、何をする?',
     my_answer: '家を買う',
     my_icon_url: '/icon_sample.jpg',
@@ -56,6 +64,7 @@ export const messages = [
     other_icon_url: '/icon_sample.jpg',
   },
   {
+    message_id: '3fa85f64-5717-4562-b3fc-2c963f66afa8',
     question_text: '学生時代の一番の思い出は何ですか？',
     my_answer: '文化祭の準備',
     my_icon_url: '/icon_sample.jpg',
@@ -63,6 +72,7 @@ export const messages = [
     other_icon_url: '/icon_sample.jpg',
   },
   {
+    message_id: '3fa85f64-5717-4562-b3fc-2c963f66afa9',
     question_text: '出身はどこですか？',
     my_answer: '東京',
     my_icon_url: '/icon_sample.jpg',
