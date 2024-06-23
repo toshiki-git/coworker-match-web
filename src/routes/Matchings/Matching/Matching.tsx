0f9f7c78fffaa2557928da932706bc7ed0816c03
Toshiki Takakubo <219tosiki@gmail.com>
2024-06-23 18:15:45 +0900

    feat: Update Matching Page

--- src/routes/Matchings/Matching/Matching.tsx
+++ src/routes/Matchings/Matching/Matching.tsx
@@ -1,8 +1,8 @@
 import { useState, useRef } from 'react';
+import useSWR from 'swr';
+import { useSession } from 'next-auth/react';
 import { Layout } from '@/layouts';
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import {
   Dialog,
   DialogClose,
@@ -13,8 +13,18 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog';
+import { Message } from '@/components/Message';
+import { fetcher } from '@/api/fetcher';
+import { MessageType } from '@/types/Message';
 
 export function MatchingPage() {
+  const { data: session } = useSession();
+  const { data: messages, error } = useSWR<MessageType[]>(
+    '/messages/matching_id', //TODO: matching_idを適切に設定する
+    fetcher
+  );
+
+  console.log(messages);
   const [questions, setQuestions] = useState<string[]>([]);
   const dialogCloseRef = useRef<HTMLButtonElement>(null);
 
@@ -38,74 +48,68 @@ export function MatchingPage() {
 
   return (
     <Layout>
-      <div className="min-h-screen bg-gray-100">
-        <main className="flex flex-col items-center mt-10">
-          <div className="mb-5">
-            AさんとBさんがマッチングしました。さっそく質問を追加してみましょう。
+      <main className="flex flex-col items-center mt-10">
+        <div className="mb-5">
+          Aさんとマッチングしました。さっそく質問を追加してみましょう。
+        </div>
+        {questions.length === 0 && (
+          <div className="mb-5 text-center text-gray-500">
+            まだ質問がありません。「質問を追加する」ボタンをクリックして質問を追加してください。
           </div>
-          {questions.length === 0 && (
-            <div className="mb-5 text-center text-gray-500">
-              まだ質問がありません。「質問を追加する」ボタンをクリックして質問を追加してください。
+        )}
+        <div>
+          {messages?.map((message, index) => (
+            <div key={index} className="mb-6">
+              <Message
+                question_text={message.question_text}
+                my_icon_url={session?.user?.image as string}
+                my_answer={message.my_answer}
+                other_icon_url={message.other_icon_url}
+                other_answer={message.other_answer}
+              />
             </div>
-          )}
-          <div className="w-full max-w-md bg-white p-4 rounded-lg shadow-md">
-            {questions.map((question, index) => (
-              <div key={index} className="mb-6">
-                <p className="text-lg font-bold text-left mb-2">
-                  質問: {question}
-                </p>
-                <div className="flex items-center mb-2">
-                  <Label className="mr-2">Bさん:</Label>
-                  <Input
-                    className="flex-1"
-                    type="text"
-                    placeholder="Bさんの回答を入力"
-                  />
-                </div>
-              </div>
-            ))}
-          </div>
+          ))}
+        </div>
 
-          <div className="flex space-x-4 mt-10">
-            <Dialog>
-              <DialogTrigger asChild>
-                <Button>質問を追加する</Button>
-              </DialogTrigger>
-              <DialogContent className="sm:max-w-md">
-                <DialogHeader>
-                  <DialogTitle>質問を追加する</DialogTitle>
-                  <DialogDescription>
-                    追加したい質問を選んでください。
-                  </DialogDescription>
-                </DialogHeader>
-                <div className="grid grid-cols-1 gap-4 mt-4">
-                  {questionOptions.map((option, index) => (
-                    <Button
-                      key={index}
-                      onClick={() => addQuestion(option)}
-                      className="text-left p-4 rounded-lg shadow-md"
-                    >
-                      {option}
-                    </Button>
-                  ))}
-                </div>
-                <DialogFooter className="sm:justify-start mt-4">
-                  <DialogClose asChild>
-                    <Button
-                      type="button"
-                      variant="secondary"
-                      ref={dialogCloseRef}
-                    >
-                      閉じる
-                    </Button>
-                  </DialogClose>
-                </DialogFooter>
-              </DialogContent>
-            </Dialog>
-            <Button>LINE友達登録する</Button>
-          </div>
-        </main>
-      </div>
+        <div className="flex space-x-4 mt-10">
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button>質問を追加する</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-md">
+              <DialogHeader>
+                <DialogTitle>質問を追加する</DialogTitle>
+                <DialogDescription>
+                  追加したい質問を選んでください。
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid grid-cols-1 gap-4 mt-4">
+                {questionOptions.map((option, index) => (
+                  <Button
+                    key={index}
+                    onClick={() => addQuestion(option)}
+                    className="text-left p-4 rounded-lg shadow-md"
+                  >
+                    {option}
+                  </Button>
+                ))}
+              </div>
+              <DialogFooter className="sm:justify-start mt-4">
+                <DialogClose asChild>
+                  <Button
+                    type="button"
+                    variant="secondary"
+                    ref={dialogCloseRef}
+                  >
+                    閉じる
+                  </Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+          <Button>LINE友達登録する</Button>
+        </div>
+      </main>
     </Layout>
   );
 }
