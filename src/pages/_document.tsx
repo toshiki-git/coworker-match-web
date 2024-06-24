88002e87625a6fb9c1d59f0b6459996b6333e41f
Toshiki Takakubo <219tosiki@gmail.com>
2024-06-24 19:39:10 +0900

    Remove unused SVG files

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="icon" href="/CwM-logo-green-transparent.png" />
       <body>
         <Main />
         <NextScript />
