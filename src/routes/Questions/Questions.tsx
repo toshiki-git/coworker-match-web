10b31ea97109f65c8a875f28419ba94d9faa354f
Toshiki Takakubo <219tosiki@gmail.com>
2024-06-26 18:10:58 +0900

    feat: add error handling for no matching

--- src/routes/Questions/Questions.tsx
+++ src/routes/Questions/Questions.tsx
@@ -13,6 +13,7 @@ export function QuestionsPage() {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [selectedChoices, setSelectedChoices] = useState<number[]>([]);
   const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const { data: session } = useSession();
   const router = useRouter();
 
@@ -40,11 +41,17 @@ export function QuestionsPage() {
         user_id: session?.userId,
         answers,
       };
-      const result = await post('/matching_questions', requestBody);
 
-      setTimeout(() => {
-        router.push(`/matchings/${result.matching_id}`);
-      }, 2000);
+      try {
+        const result = await post('/matching_questions', requestBody);
+
+        setTimeout(() => {
+          router.push(`/matchings/${result.matching_id}`);
+        }, 2000);
+      } catch (error) {
+        setLoading(false);
+        setErrorMessage('マッチングしたユーザーはいませんでした');
+      }
     }
   };
 
@@ -52,6 +59,10 @@ export function QuestionsPage() {
     if (currentQuestionIndex > 0) {
       setCurrentQuestionIndex(currentQuestionIndex - 1);
     }
+  };
+
+  const handleRetry = () => {
+    window.location.reload();
   };
 
   const currentQuestion = data[currentQuestionIndex];
@@ -64,44 +75,60 @@ export function QuestionsPage() {
   return (
     <Layout>
       <div className="w-full flex flex-col items-center gap-5 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <div className="flex gap-9 items-center">
-          <div>
-            <span className="text-4xl font-bold">
-              {currentQuestionIndex + 1}
-            </span>
-            <span className="text-xl">/{data.length}</span>
-          </div>
-          <div>
-            <p className="text-xl font-bold">あなたが友達になりたいのは...</p>
-            <p className="text-xl font-bold">You want to be friends...</p>
-          </div>
-        </div>
-        <div className="text-center mb-6">
-          <h1 className="text-3xl font-bold">
-            {currentQuestion?.question_text}
-          </h1>
-        </div>
-        <div className="flex space-x-4">
-          <ChoiceCard
-            choice_text={currentQuestion?.choice1.choice_text}
-            choice_image_url={currentQuestion?.choice1.choice_image_url}
-            isSelected={selectedChoice === 1}
-            onClick={() => handleChoiceClick(1)}
-          />
-          <ChoiceCard
-            choice_text={currentQuestion?.choice2.choice_text}
-            choice_image_url={currentQuestion?.choice2.choice_image_url}
-            isSelected={selectedChoice === 2}
-            onClick={() => handleChoiceClick(2)}
-          />
-        </div>
-        <div className="mt-6 h-12 flex items-center">
-          {currentQuestionIndex > 0 && (
-            <Button onClick={handlePreviousQuestion} className="px-4 py-2">
-              ← 前の質問に戻る
+        {errorMessage ? (
+          <div className="text-center">
+            <p className="mb-4  font-bold">{errorMessage}</p>
+            <Button
+              onClick={handleRetry}
+              className="h-16 sm:h-20 md:h-24 px-6 sm:px-8 md:px-10 py-4 sm:py-6 md:py-7 text-sm sm:text-base md:text-lg w-full"
+            >
+              新しいマッチングを開始する →
             </Button>
-          )}
-        </div>
+          </div>
+        ) : (
+          <>
+            <div className="flex gap-9 items-center">
+              <div>
+                <span className="text-4xl font-bold">
+                  {currentQuestionIndex + 1}
+                </span>
+                <span className="text-xl">/{data.length}</span>
+              </div>
+              <div>
+                <p className="text-xl font-bold">
+                  あなたが友達になりたいのは...
+                </p>
+                <p className="text-xl font-bold">You want to be friends...</p>
+              </div>
+            </div>
+            <div className="text-center mb-6">
+              <h1 className="text-3xl font-bold">
+                {currentQuestion?.question_text}
+              </h1>
+            </div>
+            <div className="flex space-x-4">
+              <ChoiceCard
+                choice_text={currentQuestion?.choice1.choice_text}
+                choice_image_url={currentQuestion?.choice1.choice_image_url}
+                isSelected={selectedChoice === 1}
+                onClick={() => handleChoiceClick(1)}
+              />
+              <ChoiceCard
+                choice_text={currentQuestion?.choice2.choice_text}
+                choice_image_url={currentQuestion?.choice2.choice_image_url}
+                isSelected={selectedChoice === 2}
+                onClick={() => handleChoiceClick(2)}
+              />
+            </div>
+            <div className="mt-6 h-12 flex items-center">
+              {currentQuestionIndex > 0 && (
+                <Button onClick={handlePreviousQuestion} className="px-4 py-2">
+                  ← 前の質問に戻る
+                </Button>
+              )}
+            </div>
+          </>
+        )}
       </div>
     </Layout>
   );
