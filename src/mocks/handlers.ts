4e16a6cc7a95092827979c741dabfe5d0053fe7f
Toshiki Takakubo <219tosiki@gmail.com>
2024-06-26 11:17:49 +0900

    refactor: Update API endpoints in mock handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -9,10 +9,10 @@ import { user_hobbies } from '@/mocks/fixtures/user_hobbies';
 
 export const handlers = [
   http.get('/resource', () => HttpResponse.json({ id: 'abc-123' })),
-  http.get('/questions', () => HttpResponse.json(questions)),
+  http.get('/matching_questions', () => HttpResponse.json(questions)),
   http.get('/hobbies', () => HttpResponse.json(hobbies)),
-  http.get('/matchings', () => HttpResponse.json(matchings)),
-  http.post('/questions', () =>
+  http.get('/matches', () => HttpResponse.json(matchings)),
+  http.post('/matching_questions', () =>
     HttpResponse.json({
       matching_id: '3fa85f64-5717-4562-b3fc-2c963f66afa0',
       sender_user_id: '3fa85f64-5717-4562-b3fc-2c963f66afa1',
