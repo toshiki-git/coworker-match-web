c496d5f9c06b2fe03c5868a45f39f8c4dbdfa30b
Toshiki Takakubo <219tosiki@gmail.com>
2024-06-27 14:20:14 +0900

    refactor: Improve MatchingsPage UI and add button for new matching

--- src/routes/Matchings/Matchings.tsx
+++ src/routes/Matchings/Matchings.tsx
@@ -3,6 +3,8 @@ import { Layout } from '@/layouts';
 import { fetcher } from '@/api/fetcher';
 import { Matchings } from '@/types/Matching';
 import useSWR from 'swr';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export function MatchingsPage() {
   const { data, error } = useSWR<Matchings[]>('/matches', fetcher);
@@ -13,16 +15,25 @@ export function MatchingsPage() {
   return (
     <Layout>
       <div className="container mt-5">
-        {data.map((matching) => (
-          <User
-            key={matching.matching_id}
-            matching_id={matching.matching_id}
-            image_url={matching.avatar_url}
-            userName={matching.name}
-            message={matching.last_message}
-            unreadCount={matching.unread_count}
-          />
-        ))}
+        {data.length === 0 ? (
+          <div className="flex flex-col items-center justify-center">
+            <p className="mb-4 text-lg">マッチングがまだありません。</p>
+            <Button className=" w-72 h-16" asChild>
+              <Link href="/questions">新しいマッチングを開始する →</Link>
+            </Button>
+          </div>
+        ) : (
+          data.map((matching) => (
+            <User
+              key={matching.matching_id}
+              matching_id={matching.matching_id}
+              image_url={matching.avatar_url}
+              userName={matching.name}
+              message={matching.last_message}
+              unreadCount={matching.unread_count}
+            />
+          ))
+        )}
       </div>
     </Layout>
   );
