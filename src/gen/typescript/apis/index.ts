676a9856e3849c03e3f822ee411a5645b3cc1513
Toshiki Takakubo <219tosiki@gmail.com>
2024-07-29 21:24:51 +0900

    feat: Add TypeScript code generation for API endpoints and models

---
+++ src/gen/typescript/apis/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './HobbiesApi';
+export * from './MatchingsApi';
+export * from './MessagesApi';
+export * from './QuestionCardsApi';
+export * from './QuestionsApi';
+export * from './UserHobbiesApi';
+export * from './UsersApi';
