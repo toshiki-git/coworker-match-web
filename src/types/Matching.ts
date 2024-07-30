c12c449ca0200f6ab895dea702598cea70e323a4
Toshiki Takakubo <219tosiki@gmail.com>
2024-07-30 16:28:13 +0900

    remove: unused type files

--- src/types/Matching.ts
+++
@@ -1,9 +0,0 @@
-export interface Matchings {
-  avatar_url: string;
-  last_message: string;
-  last_message_time: string;
-  matching_id: string;
-  name: string;
-  unread_count: number;
-  user_id: string;
-}
